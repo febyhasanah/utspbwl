c128d7f8d8e931d6f3786dbb247aca4e89325f93
Feby Hasanah Ritonga <105763368+febyhasanah@users.noreply.github.com>
2022-06-21 21:49:46 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,196 @@
+<!-- ======= Hero Section ======= -->
+<section id="hero">
+    <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+
+      <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+
+      <div class="carousel-inner" role="listbox">
+
+        <!-- Slide 1 -->
+        <div class="carousel-item active" style="background-image: url(assets/img/slide/slide-1.jpg)">
+        </div>
+
+        <!-- Slide 2 -->
+        <div class="carousel-item" style="background-image: url(assets/img/slide/slide-2.jpg)">
+        </div>
+
+        <!-- Slide 3 -->
+        <div class="carousel-item" style="background-image: url(assets/img/slide/slide-3.jpg)">
+        </div>
+
+      </div>
+
+      <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
+      </a>
+
+      <a class="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
+        <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
+      </a>
+
+    </div>
+  </section><!-- End Hero -->
+
+<section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Tentang SMAN1 TANJUNG MORAWA</h2>
+          <p> <b> SMAN1 TANJUNG MORAWA </b>adalah salah satu satuan pendidikan dengan jenjang SMA di Batang Kuis, Kec. Tanjung Morawa, Kab. Deli Serdang, Sumatera Utara. Dalam menjalankan kegiatannya, SMAN1 TANJUNG MORAWA berada di bawah naungan Kementerian Pendidikan dan Kebudayaan..</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6" data-aos="fade-right">
+            <img src="assets/img/slide/slide-2.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 content" data-aos="fade-left">
+            <h3>FASILITAS YANG DISEDIAKAN SMAN1 TANJUNG MORAWA</h3>
+            <p>
+            SMAN1 TANJUNG MORAWA menyediakan listrik untuk membantu kegiatan belajar mengajar. Sumber listrik yang digunakan oleh SMAN1 TANJUNG MORAWA berasal dari PLN. SMAN1 TANJUNG MORAWA menyediakan akses internet yang dapat digunakan untuk mendukung kegiatan belajar mengajar menjadi lebih mudah. 
+            Provider yang digunakan SMAN1 TANJUNG MORAWA untuk sambungan internetnya adalah Telkomsel Flash.
+            </p>
+            <h3>JAM PEMBELAJARAN DI SMAN1 TANJUNG MORAWA</h3>
+            <p>Pembelajaran di SMAN1 TANJUNG MORAWA dilakukan pada Pagi. Dalam seminggu, pembelajaran dilakukan selama 6 hari.</p>
+            <h3>Akreditas</h3>
+            <p>SMAN1 TANJUNG MORAWA memiliki i A, berdasarkan  860/BANSM/PROVSU/LL/XII/2018.</p>
+          </div>
+            <div class="col-lg-6 pt-4 pt-lg-0 content">
+            <ul>
+              <h3>Identitas Satuan Pendidikan</h3>
+              <li><i class="bi bi-check-circle"></i>Nama    : SMAN1 TANJUNG MORAWA</li>
+              <li><i class="bi bi-check-circle"></i>NPSN    : 10214138</li>
+              <li><i class="bi bi-check-circle"></i>Alamat  :	JL. BATANG KUIS PSR VII NO.151</li>
+              <li><i class="bi bi-check-circle"></i>Kode Pos : 20362</li>
+              <li><i class="bi bi-check-circle"></i>Kelurahan : Buntu Bedimbar</li>
+              <li><i class="bi bi-check-circle"></i>Kecamatan / Kota (LN)	: Kec. Tanjung Morawa</li>
+              <li><i class="bi bi-check-circle"></i>Kab : Kab. Deli Serdang</li>
+              <li><i class="bi bi-check-circle"></i>Provinsi : Sumatera Utara</li>
+              <li><i class="bi bi-check-circle"></i>Status Sekolah : Negeri</li>
+              <li><i class="bi bi-check-circle"></i>Jenjang Pendidikan : SMA</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section><!-- End About Us Section -->
+
+    <!-- ======= Counts Section ======= -->
+    <section id="counts" class="counts">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row no-gutters">
+
+          <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
+            <div class="count-box">
+              <i class="fa fa-user"></i>
+              <span data-purecounter-start="0" data-purecounter-end="85" data-purecounter-duration="1" class="purecounter"></span>
+              <br>
+              <strong>Siswa</strong>
+              <a href="#">For More Information &raquo;</a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
+            <div class="count-box">
+              <i class="fa fa-user"></i>
+              <span data-purecounter-start="0" data-purecounter-end="26" data-purecounter-duration="1" class="purecounter"></span>
+              <br>
+              <strong>Siswi</strong> 
+              <a href="#">For More Information &raquo;</a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
+            <div class="count-box">
+              <i class="fa fa-graduation-cap"></i>
+              <span data-purecounter-start="0" data-purecounter-end="14" data-purecounter-duration="1" class="purecounter"></span>
+              <br>
+              <strong>Alumni</strong>
+              <a href="#">For More Information &raquo;</a>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
+            <div class="count-box">
+              <i class="fas fa-award"></i>
+              <span>A</span>
+              <br>
+              <strong>Akreditas</strong>
+              <a href="#">For More Information &raquo;</a>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Counts Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Kontak</h2>
+          <p>SMAN1 TANJUNG MORAWA berada di koordinat Garis lintang: 1.5766 dan Garis bujur: 99.7351.</p>
+        </div>
+
+      </div>
+
+      <div>
+      <iframe style="border:0; width: 100%; height: 350px;"  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3982.2109470277346!2d98.78149371395294!3d3.5387302974246855!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x303139d7cd8c8fd7%3A0xd5ca7e34d116245f!2sSma%20Negeri%201%20Tj.%20Morawa%2C%20Buntu%20Bedimbar%2C%20Kec.%20Tj.%20Morawa%2C%20Kabupaten%20Deli%20Serdang%2C%20Sumatera%20Utara!5e0!3m2!1sid!2sid!4v1652797939976!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <div class="container">
+
+        <div class="row mt-5">
+
+          <div class="col-lg-6">
+
+            <div class="row">
+              <div class="col-md-12">
+                <div class="info-box">
+                  <i class="bx bx-map"></i>
+                  <h3>Alamat</h3>
+                  <p>Batang Kuis Kec. Tanjung Morawa</p>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="info-box mt-4">
+                  <i class="bx bx-envelope"></i>
+                  <h3>Email</h3>
+                  <p>febyhrtg@gmail.com</p>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="info-box mt-4">
+                  <i class="bx bx-phone-call"></i>
+                  <h3>Telepon</h3>
+                  <p>+628 316 2004 716</p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="col-lg-6">
+            <form action="#" method="post" role="form" class="php-email-form">
+              <div class="row">
+                <div class="col form-group">
+                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+                </div>
+              <div class="form-group mt-3">
+                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+              </div>
+              <div class="form-group mt-3">
+                <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+              </div>
+              <div class="my-3">
+                <div class="loading">Loading</div>
+                <div class="error-message"></div>
+                <div class="sent-message">Your message has been sent. Thank you!</div>
+              </div>
+              <div class="text-center"><button type="submit">Send Message</button></div>
+            </form>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
